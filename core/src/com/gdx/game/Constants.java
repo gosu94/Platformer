471942017533ec2f067a7d71ded05fc28d8a3758
gosu94 <Jayceon80>
2017-11-23 15:15:56 +0100

    Basic input reading added

--- core/src/com/gdx/game/Constants.java
+++ core/src/com/gdx/game/Constants.java
@@ -1,10 +1,10 @@
 package com.gdx.game;
 
-public class Constants {
+class Constants {
 
      // Visible game world is 5 meters wide
-     public static final float VIEWPORT_WIDTH = 5.0f;
+     static final float VIEWPORT_WIDTH = 10.0f;
      // Visible game world is 5 meters tall
-     public static final float VIEWPORT_HEIGHT = 5.0f;
+     static final float VIEWPORT_HEIGHT = 10.0f;
 
 }
--- core/src/com/gdx/game/WorldController.java
+++ core/src/com/gdx/game/WorldController.java
@@ -1,46 +1,46 @@
 package com.gdx.game;
 
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.MathUtils;
 
-public class WorldController {
+public class WorldController extends InputAdapter {
     private static final String TAG = WorldController.class.getName();
-    public Sprite[] testSprites;
+    Sprite[] testSprites;
     private int selectedSprite;
 
-    public WorldController() {
+    WorldController() {
         init();
     }
 
     private void init () {
+        Gdx.input.setInputProcessor(this);
         initTestObjects();
     }
 
     private void initTestObjects(){
-        // Create new array for 5 sprites
-            testSprites = new Sprite[5];
+        testSprites = new Sprite[5];
         // Create empty POT-sized Pixmap with 8 bit RGBA pixel data
-            int width = 32;
-            int height = 32;
-            Pixmap pixmap = createProceduralPixmap(width, height);
-        // Create a new texture from pixmap data
-            Texture texture = new Texture(pixmap);
-        // Create new sprites using the just created texture
-            for (int i = 0; i < testSprites.length; i++) {
-              Sprite spr = new Sprite(texture);
-        // Define sprite size to be 1m x 1m in game world
-              spr.setSize(1, 1);
+        int width = 32;
+        int height = 32;
+        Pixmap pixmap = createProceduralPixmap(width, height);
+        Texture texture = new Texture(pixmap);
+        for (int i = 0; i < testSprites.length; i++) {
+            Sprite spr = new Sprite(texture);
+            spr.setSize(1, 1);
         // Set origin to sprite's center
-              spr.setOrigin(spr.getWidth() / 2.0f, spr.getHeight() / 2.0f);
-        // Calculate random position for sprite
-              float randomX = MathUtils.random(-2.0f, 2.0f);
-              float randomY = MathUtils.random(-2.0f, 2.0f);
-              spr.setPosition(randomX, randomY);
-        // Put new sprite into array
-              testSprites[i] = spr;
-           }    // Set first sprite as selected one    selectedSprite = 0;
+            spr.setOrigin(spr.getWidth() / 2.0f, spr.getHeight() / 2.0f);
+            float randomX = MathUtils.random(-2.0f, 2.0f);
+            float randomY = MathUtils.random(-2.0f, 2.0f);
+            spr.setPosition(randomX, randomY);
+            testSprites[i] = spr;
+        }
+        selectedSprite = 0;
     }
 
     private Pixmap createProceduralPixmap(int width, int height) {
@@ -58,8 +58,41 @@ public class WorldController {
 
     }
 
-    public void update (float deltaTime) {
+    void update(float deltaTime) {
+        handleDebugInput(deltaTime);
         updateTestObjects(deltaTime);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == Keys.R) {
+            init();
+            Gdx.app.debug(TAG, "World has been resetted");
+        }
+        if (keycode == Keys.SPACE) {
+            selectedSprite = (selectedSprite + 1) % testSprites.length;
+            Gdx.app.debug(TAG, "Sprite #" + selectedSprite);
+        }
+        return false;
+    }
+
+    private void handleDebugInput(float deltaTime) {
+        if (Gdx.app.getType() != ApplicationType.Desktop) return;
+        // Selected Sprite Controls
+        float sprMoveSpeed = 5 * deltaTime;
+        if (Gdx.input.isKeyPressed(Keys.A))
+            moveSelectedSprite(-sprMoveSpeed, 0);
+        if (Gdx.input.isKeyPressed(Keys.D))
+            moveSelectedSprite(sprMoveSpeed, 0);
+        if (Gdx.input.isKeyPressed(Keys.W))
+            moveSelectedSprite(0, sprMoveSpeed);
+        if (Gdx.input.isKeyPressed(Keys.S))
+            moveSelectedSprite(0, -sprMoveSpeed);
+
+    }
+
+    private void moveSelectedSprite(float x, float y) {
+        testSprites[selectedSprite].translate(x, y);
     }
 
     private void updateTestObjects(float deltaTime) {
