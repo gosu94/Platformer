e4d8ced6bcaa49614259d7ad1be2c2f44f1538e7
gosu94 <Jayceon80>
2017-11-23 14:06:45 +0100

    First draw

--- core/src/com/gdx/game/GdxGame.java
+++ core/src/com/gdx/game/GdxGame.java
@@ -1,33 +1,41 @@
 package com.gdx.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+    private static final String TAG = GdxGame.class.getName();
+    private WorldController worldController;
+    private WorldRenderer worldRenderer;
+
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+        worldController.update(Gdx.graphics.getDeltaTime());
+		Gdx.gl.glClearColor(00/255.0f, 149/255.0f, 237/255.0f, 255/255.0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        worldRenderer.render();
 	}
+
+	@Override
+    public void resize(int width, int height){
+        worldRenderer.resize(width, height);
+    }
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+        worldRenderer.dispose();
 	}
 }
---
+++ core/src/com/gdx/game/WorldRenderer.java
@@ -0,0 +1,48 @@
+package com.gdx.game;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Disposable;
+import com.gdx.game.Constants;
+
+public class WorldRenderer implements Disposable {
+
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private WorldController worldController;
+    public WorldRenderer (WorldController worldController) {
+        this.worldController = worldController;
+        init();
+    }
+    private void init() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH,Constants.VIEWPORT_HEIGHT);
+        camera.position.set(0,0,0);
+        camera.update();
+    }
+    public void render () {
+        renderTestObejcts();
+    }
+
+    private void renderTestObejcts() {
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        for(Sprite sprite : worldController.testSprites){
+            sprite.draw(batch);
+        }
+        batch.end();
+    }
+
+
+    public void resize (int width, int height) {
+        camera.viewportWidth = (Constants.VIEWPORT_HEIGHT/height) * width;
+        camera.update();
+    }
+
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+    }
+}
