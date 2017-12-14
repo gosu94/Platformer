e64ccec062ae6ef2ffc49430537b08a1c9dfb6b3
Tomasz Pilarczyk <tomasz.pilarczyk@icloud.com>
2017-12-14 22:38:32 +0100

    Physics added

---
+++ core/src/com/gdx/game/Coin.java
@@ -0,0 +1,37 @@
+package com.gdx.game;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Coin extends AbstractGameObject {
+
+    public boolean collected;
+    private TextureRegion regGoldCoin;
+
+    public Coin() {
+        init();
+    }
+
+    private void init() {
+        dimension.set(0.5f, 0.5f);
+        regGoldCoin = Assets.instance.coin.coin;
+        // Set bounding box for collision detection
+        bounds.set(0, 0, dimension.x, dimension.y);
+        collected = false;
+    }
+
+    public void render(SpriteBatch batch) {
+        if (collected) return;
+        TextureRegion reg = null;
+        reg = regGoldCoin;
+        batch.draw(reg.getTexture(), position.x, position.y,
+                origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y,
+                rotation, reg.getRegionX(), reg.getRegionY(),
+                reg.getRegionWidth(), reg.getRegionHeight(), false, false);
+    }
+
+    public int getScore() {
+        return 100;
+    }
+
+}
