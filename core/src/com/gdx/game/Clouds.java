bb8161e83e8bb1fee5a8f6baaca72c17260e9e14
Tomasz Pilarczyk <tomasz.pilarczyk@icloud.com>
2018-01-06 15:52:40 +0100

    Factory method update

--- core/src/com/gdx/game/Clouds.java
+++ core/src/com/gdx/game/Clouds.java
@@ -8,57 +8,23 @@ public class Clouds extends AbstractGameObject {
     private float length;
     private Array<Cloud> clouds;
 
-//    private class Cloud extends AbstractGameObject {
-//        private TextureRegion regCloud;
-//
-//        public Cloud() {
-//        }
-//
-//        public void setRegion(TextureRegion region) {
-//            regCloud = region;
-//        }
-//
-//        @Override
-//        public void render(SpriteBatch batch) {
-//            TextureRegion reg = regCloud;
-//            batch.draw(reg.getTexture(), position.x + origin.x, position.y + origin.y, origin.x, origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(), false, false);
-//        }
-//    }
-
-
     public Clouds(float length) {
         this.length = length;
-        init();
+        fillLevel();
     }
 
-    private void init() {
+    private void fillLevel() {
         dimension.set(2.0f, 1.0f);
         int distFac = 5;
         int numClouds = (int) (length / distFac);
         clouds = new Array<Cloud>(2 * numClouds);
         for (int i = 0; i < numClouds; i++) {
-            Cloud cloud = EnvironmentFactroy.createCloud(i * 10, dimension);//spawnCloud();
+            Cloud cloud = (Cloud) EnvironmentFactroy.createCloud("Cloud", i * 10, 0, dimension);//spawnCloud();
             cloud.position.x = i * distFac;
             clouds.add(cloud);
         }
     }
 
-//    private Cloud spawnCloud() {
-//        Cloud cloud = new Cloud();
-//        cloud.dimension.set(dimension);
-//        // select random cloud image
-//        cloud.setRegion(Assets.instance.levelDecoration.cloud);
-//        // position
-//        Vector2 pos = new Vector2();
-//        pos.x = length + 10;
-//        // position after end of level
-//        pos.y += 1.75;
-//        // base position
-//        pos.y += MathUtils.random(0.0f, 0.2f) * (MathUtils.randomBoolean() ? 1 : -1);
-//        // random additional position
-//        cloud.position.set(pos);
-//        return cloud;
-//    }
 
     @Override
     public void render(SpriteBatch batch) {
--- core/src/com/gdx/game/EnvironmentFactroy.java
+++ core/src/com/gdx/game/EnvironmentFactroy.java
@@ -4,15 +4,21 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class EnvironmentFactroy {
-    public static Cloud createCloud(int posX, Vector2 dimension) {
-        Cloud cloud = new Cloud();
-        cloud.dimension.set(dimension);
-        cloud.setRegion(Assets.instance.levelDecoration.cloud);
-        Vector2 pos = new Vector2();
-        pos.x = posX;
-        pos.y += 5.75;
-        pos.y += MathUtils.random(0.0f, 0.2f) * (MathUtils.randomBoolean() ? 1 : -1);
-        cloud.position.set(pos);
-        return cloud;
+    public static AbstractGameObject createCloud(String object, int posX, int posY, Vector2 dimension) {
+
+        if ("Cloud".equals(object)) {
+            Cloud cloud = new Cloud();
+            cloud.dimension.set(dimension);
+            cloud.setRegion(Assets.instance.levelDecoration.cloud);
+            Vector2 pos = new Vector2();
+            pos.x = posX;
+            pos.y += 5.75;
+            pos.y += MathUtils.random(0.0f, 0.2f) * (MathUtils.randomBoolean() ? 1 : -1);
+            cloud.position.set(pos);
+            return cloud;
+        }
+
+        return null;
+
     }
 }
